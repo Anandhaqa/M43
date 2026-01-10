4761298eaedb86c4d3939e39178345d15af4bdbc
Anandhakrishnan <anandhaqa@gmail.com>
2026-01-10 08:20:56 +0530

    Commit

---
+++ M43demo/src/test/java/dummy/sample.java
@@ -0,0 +1,21 @@
+package dummy;
+
+public class sample {
+	public static void main(String[] args) {
+		System.out.println("Dummy Project");
+	}
+}
+
+
+//1. Right Click on Project
+//2. Go to Team ---Share Project
+//3.click create
+//branch name as master--finish--finish
+//Right Click on Project ---Go To Teams---commit
+//maximize popup --- click on ++ button
+//commit---click on commit and push---ok
+//click on pushagead
+//1st git url --- paste in url part
+//password---token id
+//user ---username in git
+//preview--preview--push
