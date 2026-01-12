a7ccf221610b51c281bada9608c9bbb44900cf06
Anandhakrishnan <anandhaqa@gmail.com>
2026-01-12 07:57:48 +0530

    Junior1

---
+++ M43demo/src/test/java/dummy/Junior1.java
@@ -0,0 +1,11 @@
+package dummy;
+
+public class Junior1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Junior1 is trying to add new Program and push into GitHub");
+		
+	}
+
+}
